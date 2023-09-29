772056773cb6edc10c5dd7793700723d9eb0eaaa
augustoroocha <augustinho_darocha1311@hotmail.com>
2023-09-29 14:13:53 -0300

    add

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ** RPC ( *Remote Procedure Call* )** em Python
 
-
 Este arquivo consiste em três exercícios, o primeiro exercício calcula o IMC de uma pessoa " **IMC = peso / (altura x altura)**. ", com isso conseguimos identificar se a pessoa têm algum problema de saúde.
 
 o arquivo *imc/cliente.py* * chama a função imc que está no *imc/servidor.py* enviando peso e altura
@@ -10,3 +9,14 @@ o arquivo *imc/cliente.py* * chama a função imc que está no *imc/servidor.py*
  ***https://www.tuasaude.com/calculadora/imc/?authuser=1*'**
 
 Por exemplo se o valor do seu IMC for menor que 18.5 a sua classificação é considerada como 'Magreza'
+
+
+
+Palíndromo 
+
+este aquivo faz uma função que captura uma palavra , se a palavra invertida for possível fazer a leitura igual a palavra escrita normalmente significa que é um Palíndromo, por exemplo a palavra ' level ' em inglês , pode ser lida de trás para frente tendo o mesmo resultado.
+
+
+Equação de segundo grau
+
+neste aquivo é realizado uma conta , basta o cliente digitar três números válidos e logo irá retornar o resultado da equação
--- equação/servidor.py
+++ equação/servidor.py
@@ -1,29 +1,25 @@
 import xmlrpc.server
 
-def calcular_raizes_equacao_segundo_grau(a, b, c):
-    discriminante = b**2 - 4*a*c
+def equacao(a, b, c):
+    d = b**2 - 4*a*c
     
-    if discriminante > 0:
-        x1 = (-b + discriminante**0.5) / (2*a)
-        x2 = (-b - discriminante**0.5) / (2*a)
-        raizes = [x1, x2]
-    elif discriminante == 0:
-        x1 = -b / (2*a)
-        raizes = [x1]
+    if d > 0:
+        valX1 = (-b + d**0.5) / (2*a)
+        valX2 = (-b - d**0.5) / (2*a)
+        r = [valX1, valX2]
+    elif d == 0:
+        valX1 = -b / (2*a)
+        r = [valX1]
     else:
         parte_real = -b / (2*a)
-        parte_imaginaria = (-discriminante)**0.5 / (2*a)
-        raizes = [f"{parte_real} + {parte_imaginaria}i", f"{parte_real} - {parte_imaginaria}i"]
+        parte_imaginaria = (-d)**0.5 / (2*a)
+        r = [f"{parte_real} + {parte_imaginaria}i", f"{parte_real} - {parte_imaginaria}i"]
 
-    return raizes
+    return r
 
 def main():
-    # Crie um servidor RPC
     with xmlrpc.server.SimpleXMLRPCServer(("localhost", 8000)) as server:
-        server.register_function(calcular_raizes_equacao_segundo_grau, "calcular_raizes_equacao_segundo_grau")
-        
-        print("Servidor RPC pronto para receber chamadas...")
+        server.register_function(equacao, "equacao")
         server.serve_forever()
-
 if __name__ == "__main__":
     main()
